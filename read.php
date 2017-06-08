618f36ec7b966a54a61fd3f8c2cc1cbab1f7278d
David Petringa <david@ajaxtransport.com>
2017-06-08 10:18:13 -0400

    Added meta description and key words

--- read.php
+++ read.php
@@ -6,8 +6,8 @@
 		?>
 		<title>Read the stories | <?php echo TITLE; ?></title>
 		<meta charset="UTF-8">
-		<meta name="description" content="">
-		<meta name="keywords" content="">
+		<meta name="description" content="I wanted to code a website and practice using PHP and MySQL for the backend. This is the end result.">
+		<meta name="keywords" content="David Petringa, MySQL, PHP, Front end, Back end, HTML, CSS, website development.">
 		<meta name="author" content="David Petringa">
 
 		<?php
